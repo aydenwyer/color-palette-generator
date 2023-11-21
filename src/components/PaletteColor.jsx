e48adf2c5f9e8a74c4255ba9f69608f7146698b3
Ayden Wyer <102749343+aydenwyer@users.noreply.github.com>
2023-11-20 19:16:38 -0600

    Adding another API endpoint to get info for each color

--- src/components/PaletteColor.jsx
+++ src/components/PaletteColor.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const PaletteColor = ({color}) => {
+  const [name, setName] = useState("");
   
+  useEffect(() => {
+    const url = `https://www.thecolorapi.com/id?rgb=${color.toString()}&format=json`;
+
+    fetch(url)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setName(data.name.value)
+      })
+  }, [color]);
 
   return (
-    <div className="w-[100px] h-[100px]" style={{backgroundColor: "rgb(" + color.toString() + ")"}}>{color.toString()}</div>
+    <>{name && <div className="w-[100px] h-[100px]" style={{backgroundColor: "rgb(" + color.toString() + ")"}}>{name}</div>}</>
   )
 }
 
